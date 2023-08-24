869adff07ed9544be86c7b09b85b0546697d1200
Dmitriy Karpievich <todeath1987@gmail.com>
2023-08-24 14:26:55 +0300

    добавил ДЗ №2

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Task2 {
+
+//    Вычисление високосного года
+
     public static void main(String[] args) {
-какие то изменения
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите год для проверки на високосность");
+        int year = 0;
+        boolean chekPositive = true;
+        while (chekPositive) {
+            while (!sc.hasNextInt()) {
+                System.out.println("Вы ввели какую-то дичь. Попробуйте еще раз ввести год");
+                sc.next();
+            }
+            year = sc.nextInt();
+            if (year >= 0) chekPositive = false;
+            else System.out.println("Вы ввели отрицательное число. Попробуйте еще раз");
+        }
+        if (year%4 == 0){
+            if (year%100 == 0 && year%400 != 0) {
+                System.out.println(year + " не является високосным");
+                return;
+            }
+            System.out.println(year + " является високосным");
+        } else System.out.println(year + " не является високосным");
     }
 }
