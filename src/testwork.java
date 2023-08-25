5c739ac0e4a65c861a4fbf9ecc3ce9461969b2ea
Abramov Ivan <todeath1987@gmail.com>
2023-08-25 21:12:10 +0300

    зациклил программу

--- src/testwork.java
+++
@@ -1,9 +0,0 @@
-public class testwork {
-    int value = 14783346;
-    int result = 0;
-    int checkNull = value;
-    for (int i = 0, checkNull == 0, i++ ){
-        result += value%10;
-        checkNull= value/10;
-    }
-}
