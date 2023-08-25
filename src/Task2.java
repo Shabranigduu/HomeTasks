cff9520122ab1d5ae932caa4ccea443ce87f72fc
Abramov Ivan <82610035+Vainir777@users.noreply.github.com>
2023-08-25 20:22:19 +0300

    зациклил программу

--- src/Task2.java
+++ src/Task2.java
@@ -6,24 +6,33 @@ public class Task2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите год для проверки на високосность");
+
         int year = 0;
-        boolean chekPositive = true;
-        while (chekPositive) {
-            while (!sc.hasNextInt()) {
-                System.out.println("Вы ввели какую-то дичь. Попробуйте еще раз ввести год");
-                sc.next();
+        String string;
+        while (true) {
+            System.out.println("Введите год для проверки на високосность");
+            while (true) {
+                while (!sc.hasNextInt()) {
+                    System.out.println("Вы ввели какую-то дичь. Попробуйте еще раз ввести год");
+                    sc.next();
+                }
+                year = sc.nextInt();
+                if (year >= 0) {
+                    break;
+                } else {
+                    System.out.println("Вы ввели отрицательное число. Попробуйте еще раз");
+                }
             }
-            year = sc.nextInt();
-            if (year >= 0) chekPositive = false;
-            else System.out.println("Вы ввели отрицательное число. Попробуйте еще раз");
-        }
-        if (year%4 == 0){
-            if (year%100 == 0 && year%400 != 0) {
+            if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)) {
+                System.out.println(year + " является високосным");
+            } else {
                 System.out.println(year + " не является високосным");
-                return;
             }
-            System.out.println(year + " является високосным");
-        } else System.out.println(year + " не является високосным");
+            System.out.println("Для выхода напиште \"выход\" или \"в\". Для продолжения напишите любой другой текст");
+            string = sc.next();
+            if (string.equalsIgnoreCase("в") || string.equalsIgnoreCase("выход")){
+                break;
+            }else System.out.println("продолжаем");
+        }
     }
 }
