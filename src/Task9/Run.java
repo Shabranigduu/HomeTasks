bc175a1e22aea2d6ba0e80aeccf9c2015ea9a437
Dmitriy Karpievich <todeath1987@gmail.com>
2023-09-10 18:51:47 +0300

    Добавил первую часть ДЗ №9

--- src/Task9/Run.java
+++ src/Task9/Run.java
@@ -1,36 +1,38 @@
 package Task9;
 
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Run {
-    public static void main(String[] args) throws IOException {
-        File in = new File("prose.txt");
-        File out = new File("proseChars.txt");
-        out.createNewFile();
-        PrintWriter printWriter = new PrintWriter(out);
-
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(in));
-        StringBuilder prose = new StringBuilder();
-        while (bufferedReader.ready()) {
-            prose.append(bufferedReader.readLine());
+    public static void main(String[] args) {
+        Path in = Path.of("prose.txt");
+        Path out = Path.of("proseChars.txt");
+        try (BufferedReader bufferedReader = Files.newBufferedReader(in)) {
+            StringBuilder prose = new StringBuilder();
+            while (bufferedReader.ready()) {
+                prose.append(bufferedReader.readLine());
+            }
+            String str = prose.toString().toLowerCase().replaceAll("[^а-яА-Я]", "");
+            System.out.println(str);
+            Map<Character, Integer> chars = new TreeMap<>();
+            for (char cha : str.toCharArray()) {
+                addChar(cha, chars);
+            }
+            for (Map.Entry<Character, Integer> characterIntegerEntry : chars.entrySet()) {
+                Files.writeString(out, characterIntegerEntry.getKey().toString() + " - " + characterIntegerEntry.getValue() + "\n", StandardOpenOption.APPEND);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файла prose.txt не существует");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        String str = prose.toString().toLowerCase().replaceAll("[^а-яА-Я]", "");
-        System.out.println(str);
-        Map<Character, Integer> chars = new TreeMap<>();
-
-        for (char cha : str.toCharArray()) {
-            addChar(cha, chars);
-        }
-        for (Map.Entry<Character, Integer> characterIntegerEntry : chars.entrySet()) {
-            printWriter.println(characterIntegerEntry.getKey().toString()+" - "+ characterIntegerEntry.getValue());
-        }
-        printWriter.close();
-
     }
-
     public static void addChar(Character cha, Map<Character, Integer> mapOfChars) {
         if (mapOfChars.containsKey(cha)) {
             mapOfChars.put(cha, mapOfChars.get(cha) + 1);
